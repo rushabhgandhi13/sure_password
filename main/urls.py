b908409e53e0ca576ff3ead7516d09589e66bafd
nixen2802 <n.mandliya@somaiya.edu>
2022-02-07 16:59:34 +0530

    Functionalities done

--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('service/<int:pk>/',views.service,name='service'),
     path('delete/<int:pk>/',views.delete,name='delete'),
+    path('view_password/<int:pk>',views.view_password, name="view_password")
     ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
+from doctest import master
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import *
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 
 # Create your views here.
 def home(request):
@@ -12,13 +16,19 @@ def service(request,pk):
     context={
         'service' : service,
     }
-    if(request.method == 'POST'):
-        user = Service()
-        user.service_name = request.POST.get('name')
-        user.user = cur_user
-        user.password = request.POST.get('password')
-        user.save()
-
+    if(request.method == 'POST' and request.POST.get('checker')=="add_service"):
+        service = Service()
+        service.service_name = request.POST.get('name')
+        service.user = cur_user
+        service.password = encrypt_password(cur_user.key,request.POST.get('password')).decode()
+        service.save()
+    if(request.method == 'POST' and request.POST.get('checker')=="update_service"):
+        Service.objects.filter(id = request.POST.get('id')).update(service_name=request.POST.get('name'),password = encrypt_password(cur_user.key,request.POST.get('password')).decode())
+    if(request.method == 'POST' and request.POST.get('checker')=="delete_service"):
+        query = User.objects.get(id=cur_user.id)
+        if(request.POST.get('password')==decrypt_password(cur_user.key,cur_user.master_password)):
+            query.delete()
+            return redirect('home')
     return render(request, 'main/service.html', context)
 
 def login(request):
@@ -28,7 +38,8 @@ def login(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         for i in user:
-            if(i.email==email and i.master_password== password):
+            # print(i.key.encode(),i.master_password.endcode())
+            if(i.email==email and decrypt_password(i.key,i.master_password)== password):
                 user_id = i.id
                 return redirect('service', pk=user_id)
     return render(request, 'main/login.html')
@@ -38,16 +49,39 @@ def register(request):
         user = User()
         user.name = request.POST.get('name')
         user.email = request.POST.get('email')
-        user.master_password = request.POST.get('password')
-        user.key = '1234'
+        user.key = Fernet.generate_key()
+        master_pass = request.POST.get('password')
+        user.master_password=encrypt_password(user.key,master_pass).decode()
+        user.key=user.key.decode()
         # print(name, email, master_password)
         user.save()
         return redirect('login')
     return render(request, 'main/register.html')
 
+def view_password(request,pk):
+    s=Service.objects.get(id=pk)
+    u=s.user.key
+    temp=decrypt_password(u,s.password)
+    context={
+        'name': s.service_name,
+        'password': temp
+    }
+    return render(request,'main/view_service.html', context)
 def delete(request, pk):
     query = Service.objects.get(id=pk)
     cur_user1 = User.objects.get(id= query.user.id)
     uid = cur_user1.id
     query.delete()
     return redirect('service', pk=uid)
+
+
+def encrypt_password(key, password):
+    f = Fernet(key)
+    token = f.encrypt(password.encode())
+    return token
+
+
+def decrypt_password(key, encrypted_password):
+    f = Fernet(key)
+    password = f.decrypt(encrypted_password.encode())
+    return password.decode()
